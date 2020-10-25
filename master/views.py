ae61c37d46b5c8be23700ce735294f657a76d9cd
Aditya Nanda Tri Prakoso <adityanandaaa@gmail.com>
2020-10-25 20:36:48 +0700

    get login user

--- master/views.py
+++ master/views.py
@@ -2,9 +2,20 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+from master.models import *
+
+
 @login_required(login_url='login')
 def redirect(request):
     return render(request, "profile/redirect.html")
 
+@login_required(login_url='login')
 def home(request):
-    return render(request,"home.html")
+    current_user = request.user
+
+    data = Profile.objects.all()
+
+    profile = {
+        "profile_all": current_user
+    }
+    return render(request,"home.html",profile)
